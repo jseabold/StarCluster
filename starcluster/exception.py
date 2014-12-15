24e33d7cd00f506704f9605355471872be66a5e5
Alex Rothberg <agrothberg@gmail.com>
2014-12-15 18:37:41 -0500

    force str conversion on error messages

--- starcluster/exception.py
+++ starcluster/exception.py
@@ -532,7 +532,7 @@ class ThreadPoolException(BaseException):
         excs = []
         for exception in self.exceptions:
             e, tb_msg, jobid = exception
-            excs.append('error occurred in job (id=%s): %s' % (jobid, str(e)))
+            excs.append('error occurred in job (id=%s): %s' % (str(jobid), str(e)))
             excs.append(tb_msg)
         return '\n'.join(excs)
 
