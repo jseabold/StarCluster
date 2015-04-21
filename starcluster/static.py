66a9ebc5b1c14cd03844c424c2972bdcfa2349a5
Duncan McGreggor <oubiwann@gmail.com>
2015-04-21 11:28:51 -0500

    Updated version.

--- starcluster/static.py
+++ starcluster/static.py
@@ -55,7 +55,7 @@ def create_sc_config_dirs():
     __makedirs(STARCLUSTER_LOG_DIR)
 
 
-VERSION = "0.95.6"
+VERSION = "0.95.7+py3k-oubiwann"
 PID = os.getpid()
 TMP_DIR = tempfile.gettempdir()
 if os.path.exists("/tmp"):
